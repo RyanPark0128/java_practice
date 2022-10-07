4f8b39241575987c4a7a421451169c89d6799153
RyanPark0128 <vazywon@hotmail.com>
2022-10-07 13:01:22 -0700

    practice

---
+++ practice.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Practice {
+  public static void main(String[] args) {
+    System.out.print("sadklasndklnasdksad");
+
+    System.out.print("  yes");
+  }
+}
