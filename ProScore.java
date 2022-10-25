affe7cfc663dfc1450b6c482db81b095a08903a6
RyanPark0128 <vazywon@hotmail.com>
2022-10-25 11:58:01 -0700

    assignment 5

---
+++ ProScore.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class ProScore {
+    public static String readInSequence(Scanner sc) {
+        System.out.println("Enter a sequence");
+        String input;
+        while (true) {
+            input = sc.nextLine();
+            if (isValidSequence(input)) {
+                break;
+            }
+            System.out.println("Invalid sequence, Try Again");
+        }
+        return input;
+    }
+
+    public static boolean isValidSequence(String seq) {
+        String pSequence = "ARNDCQEGHILKMFPSTWYVBZX*";
+        String[] arrayOfSequence = seq.toUpperCase().split("");
+        for (int i = 0; i < seq.length(); i++) {
+            if (pSequence.indexOf(arrayOfSequence[i]) == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static String makeEqualLength(String seq, int toAdd) {
+        for (int i = 0; i < toAdd; i++) {
+            seq += "*";
+        }
+        return seq;
+    }
+
+    public static int scoreNucleotides(String seq1, String seq2, int[][] blosum62) {
+        return 123;
+    }
+
+    public static int proToInder(char protein) {
+        return 123;
+    }
+
+    public static void main(String[] args) {
+        int[][] blosum62 = {
+                { 4, -1, -2, -2, 0, -1, -1, 0, -2, -1, -1, -1, -1, -2, -1, 1, 0, -3, -2, 0, -2, -1, 0, -4 },
+                { -1, 5, 0, -2, -3, 1, 0, -2, 0, -3, -2, 2, -1, -3, -2, -1, -1, -3, -2, -3, -1, 0, -1, -4 },
+                { -2, 0, 6, 1, -3, 0, 0, 0, 1, -3, -3, 0, -2, -3, -2, 1, 0, -4, -2, -3, 3, 0, -1, -4 },
+                { -2, -2, 1, 6, -3, 0, 2, -1, -1, -3, -4, -1, -3, -3, -1, 0, -1, -4, -3, -3, 4, 1, -1, -4 },
+                { 0, -3, -3, -3, 9, -3, -4, -3, -3, -1, -1, -3, -1, -2, -3, -1, -1, -2, -2, -1, -3, -3, -2, -4 },
+                { -1, 1, 0, 0, -3, 5, 2, -2, 0, -3, -2, 1, 0, -3, -1, 0, -1, -2, -1, -2, 0, 3, -1, -4 },
+                { -1, 0, 0, 2, -4, 2, 5, -2, 0, -3, -3, 1, -2, -3, -1, 0, -1, -3, -2, -2, 1, 4, -1, -4 },
+                { 0, -2, 0, -1, -3, -2, -2, 6, -2, -4, -4, -2, -3, -3, -2, 0, -2, -2, -3, -3, -1, -2, -1, -4 },
+                { -2, 0, 1, -1, -3, 0, 0, -2, 8, -3, -3, -1, -2, -1, -2, -1, -2, -2, 2, -3, 0, 0, -1, -4 },
+                { -1, -3, -3, -3, -1, -3, -3, -4, -3, 4, 2, -3, 1, 0, -3, -2, -1, -3, -1, 3, -3, -3, -1, -4 },
+                { -1, -2, -3, -4, -1, -2, -3, -4, -3, 2, 4, -2, 2, 0, -3, -2, -1, -2, -1, 1, -4, -3, -1, -4 },
+                { -1, 2, 0, -1, -3, 1, 1, -2, -1, -3, -2, 5, -1, -3, -1, 0, -1, -3, -2, -2, 0, 1, -1, -4 },
+                { -1, -1, -2, -3, -1, 0, -2, -3, -2, 1, 2, -1, 5, 0, -2, -1, -1, -1, -1, 1, -3, -1, -1, -4 },
+                { -2, -3, -3, -3, -2, -3, -3, -3, -1, 0, 0, -3, 0, 6, -4, -2, -2, 1, 3, -1, -3, -3, -1, -4 },
+                { -1, -2, -2, -1, -3, -1, -1, -2, -2, -3, -3, -1, -2, -4, 7, -1, -1, -4, -3, -2, -2, -1, -2, -4 },
+                { 1, -1, 1, 0, -1, 0, 0, 0, -1, -2, -2, 0, -1, -2, -1, 4, 1, -3, -2, -2, 0, 0, 0, -4 },
+                { 0, -1, 0, -1, -1, -1, -1, -2, -2, -1, -1, -1, -1, -2, -1, 1, 5, -2, -2, 0, -1, -1, 0, -4 },
+                { -3, -3, -4, -4, -2, -2, -3, -2, -2, -3, -2, -3, -1, 1, -4, -3, -2, 11, 2, -3, -4, -3, -2, -4 },
+                { -2, -2, -2, -3, -2, -1, -2, -3, 2, -1, -1, -2, -1, 3, -3, -2, -2, 2, 7, -1, -3, -2, -1, -4 },
+                { 0, -3, -3, -3, -1, -2, -2, -3, -3, 3, 1, -2, 1, -1, -2, -2, 0, -3, -1, 4, -3, -2, -1, -4 },
+                { -2, -1, 3, 4, -3, 0, 1, -1, 0, -3, -4, 0, -3, -3, -2, 0, -1, -4, -3, -3, 4, 1, -1, -4 },
+                { -1, 0, 0, 1, -3, 3, 4, -2, 0, -3, -3, 1, -1, -3, -1, 0, -1, -3, -2, -2, 1, 4, -1, -4 },
+                { 0, -1, -1, -1, -2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2, 0, 0, -2, -1, -1, -1, -1, -1, -4 },
+                { -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, 1 } };
+        Scanner sc = new Scanner(System.in);
+        String sequenceOne = readInSequence(sc);
+        String sequenceTwo = readInSequence(sc);
+        if (sequenceOne.length() > sequenceTwo.length()) {
+            sequenceTwo = makeEqualLength(sequenceTwo, sequenceOne.length() - sequenceTwo.length());
+        } else if (sequenceOne.length() < sequenceTwo.length()) {
+            sequenceOne = makeEqualLength(sequenceOne, sequenceTwo.length() - sequenceOne.length());
+        }
+
+    }
+
+}
