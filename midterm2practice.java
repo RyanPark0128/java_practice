3c8e2cc952231601382ade9af6618f16a28f675d
RyanPark0128 <vazywon@hotmail.com>
2022-11-10 13:03:41 -0800

    midterm practice

---
+++ midterm2practice.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class midterm2practice {
+  public static void main(String[] args) throws FileNotFoundException {
+    PrintWriter output = new PrintWriter("test2.txt");
+    output.println("yes");
+
+    output.close();
+  }
+}
