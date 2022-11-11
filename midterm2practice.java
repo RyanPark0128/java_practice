9c8a2e7c1ec11a808a92403dfbfd5c058044e961
RyanPark0128 <vazywon@hotmail.com>
2022-11-11 14:38:19 -0800

    initialize assignment 7

--- midterm2practice.java
+++ midterm2practice.java
@@ -1,11 +1,30 @@
-import java.util.*;
-import java.io.*;
+import java.util.Arrays;
 
 public class midterm2practice {
-  public static void main(String[] args) throws FileNotFoundException {
-    PrintWriter output = new PrintWriter("test2.txt");
-    output.println("yes");
+  public static void main(String[] args) {
+    // int[][] matrix = { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14,
+    // 15 }, { 16, 17, 18, 19, 20 },
+    // { 21, 22, 23, 24, 25 } };
+    // for (int i = 0; i < matrix.length; i++) {
+    // for (int j = 0; j < matrix[i].length; j++) {
+    // if (i > j) {
+    // int temp = matrix[j][i];
+    // matrix[j][i] = matrix[i][j];
+    // matrix[i][j] = temp;
+    // }
+    // }
+    // }
 
-    output.close();
+    // for (int i = 0; i < matrix.length; i++) {
+    // System.out.println(Arrays.toString(matrix[i]));
+    // }
+    int num = 278;
+    String converted = "";
+    while (num != 0) {
+      converted = String.valueOf(num % 8) + converted;
+      num = num / 8;
+    }
+
+    System.out.println(converted);
   }
 }
