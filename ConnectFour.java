c3bbd8fa64296a22d53fc0632b068ed1fef1b8e7
RyanPark0128 <vazywon@hotmail.com>
2022-11-12 15:22:12 -0800

    initialized method for board status

--- ConnectFour.java
+++ ConnectFour.java
@@ -1,5 +1,28 @@
 public class ConnectFour {
   public static void main(String[] args) {
+    String[][] board = new String[6][7];
 
+    // while (true) {
+    boardStatus(board);
+    // }
+  }
+
+  public static void boardStatus(String[][] board) {
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        String temp = "";
+        if (board[i][j] == null) {
+          temp = "-";
+        } else {
+          temp = board[i][j];
+        }
+        System.out.print("|" + temp);
+      }
+      System.out.println("|");
+    }
+    for (int i = 0; i < board[0].length; i++) {
+      System.out.print("|" + (i + 1));
+    }
+    System.out.println("|");
   }
 }
