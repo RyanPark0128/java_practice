0253a4a5dceec89cd669f451ee309d51238600bf
RyanPark0128 <vazywon@hotmail.com>
2022-11-16 22:03:26 -0800

    add comments to the code

--- ConnectFour.java
+++ ConnectFour.java
@@ -2,21 +2,42 @@ import java.util.Scanner;
 
 public class ConnectFour {
   public static void main(String[] args) {
+    // First,initialize the game board with nested arrays with string values(6 by
+    // 7), as well as the scanner
     String[][] board = new String[6][7];
     Scanner scan = new Scanner(System.in);
+
+    // initialize and declare players into variables.
     String player1 = "R";
     String player2 = "Y";
+
+    // Then, calls boardStatus method which displays the status of the gameboard.
+    // This call will show players empty game board to start with.
     boardStatus(board);
+
+    // Start the while loop to begin the game.
     while (true) {
+      // Instruction message for players
       System.out.println("It is " + player1 + " players turn");
       System.out.println("Pick a column");
+
+      // validInput method will validate the user input and will try to place the disk
+      // using placeDisk method. If there are errors, the method will ask for new user
+      // input until there aren't any errors
       validInput(scan, board, player1);
+
+      // This boardStatus call will confirm the players with disc placement.
       boardStatus(board);
+
+      // After each turn by a player, checkWinner method will be called to check if
+      // there is a winner. If it finds winner, winner gets notified and exit the
+      // while loop to end the game.
       if (checkWinner(board, player1)) {
         System.out.println(player1 + " Wins");
         break;
       }
 
+      // Now, it is second player's turn. Same process with second player.
       System.out.println("It is " + player2 + " players turn");
       System.out.println("Pick a column");
       validInput(scan, board, player2);
@@ -26,12 +47,17 @@ public class ConnectFour {
         break;
       }
     }
-
+    // if the game ends, closes the scanner and terminate.
     scan.close();
   }
 
   public static void boardStatus(String[][] board) {
+    // This empty line is to make game board more visible
     System.out.println(" ");
+
+    // nested loops are used to draw the game board. if the array value is null (not
+    // declared), the loop will assign - value to it. Because no value is assigned
+    // at the beginning, the board will start empty.
     for (int i = 0; i < board.length; i++) {
       for (int j = 0; j < board[i].length; j++) {
         String temp = "";
@@ -44,6 +70,8 @@ public class ConnectFour {
       }
       System.out.println("|");
     }
+
+    // This loop is to add row which represent the column number at the bottom.
     for (int i = 0; i < board[0].length; i++) {
       System.out.print("|" + (i + 1));
     }
@@ -51,13 +79,22 @@ public class ConnectFour {
   }
 
   public static Boolean placeDisk(String[][] board, int column, String player) {
+    // placeDisk method will try to place disk into the board. If the column is
+    // full, it will return false asking user to select different column. Otherwise
+    // it will place the disk and update the board array
     for (int i = 0; i < board.length; i++) {
       if (i == 0 && board[i][column - 1] != null) {
         return false;
-      } else if (board[i][column - 1] != null) {
+      }
+      // If the array item is not null (which means it has disk in that place), the
+      // disk will be placed one row above it.
+      else if (board[i][column - 1] != null) {
         board[i - 1][column - 1] = player;
         break;
-      } else if (i == board.length - 1 && board[i][column - 1] == null) {
+      }
+      // if there is nothing at the bottom of the gameboard, it will be placed with
+      // the disk.
+      else if (i == board.length - 1 && board[i][column - 1] == null) {
         board[i][column - 1] = player;
         break;
       }
