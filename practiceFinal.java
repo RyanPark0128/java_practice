a524393d05ef9fa11f684036d6dcf11da85674bd
RyanPark0128 <vazywon@hotmail.com>
2022-11-26 19:55:24 -0800

    java final practice 2

--- practiceFinal.java
+++ practiceFinal.java
@@ -1,5 +1,10 @@
+import java.util.Random;
+
 public class practiceFinal {
   public static void main(String[] args) {
-    System.out.println(Math.floor(3.4));
+    // System.out.println(Math.floor(3.4));
+    // Random generate = new Random();
+    // System.out.println(generate.nextFloat(100));
+    System.out.println(Math.random());
   }
 }
